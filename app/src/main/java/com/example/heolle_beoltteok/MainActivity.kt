2552068f75e54d9510e2680326fef9a72f44ad34
Your Name <you@your-domain.com>
2021-05-18 16:15:35 +0900

    test2

--- app/src/main/java/com/example/heolle_beoltteok/MainActivity.kt
+++ app/src/main/java/com/example/heolle_beoltteok/MainActivity.kt
@@ -8,5 +8,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-    fun init() {}
+    fun init() {var x:Int=1}
 }
