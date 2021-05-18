3726d4796e84371d2f36dece197cd31325dff250
coaspe <aspalt85@gmail.com>
2021-05-18 17:47:34 +0900

    base

--- app/src/main/java/com/example/heolle_beoltteok/MainActivity.kt
+++ app/src/main/java/com/example/heolle_beoltteok/MainActivity.kt
@@ -2,14 +2,49 @@ package com.example.heolle_beoltteok
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val bottomNavBar = findViewById<BottomNavigationView>(R.id.bottom_navigation)
 
+        replaceFragment(HomeFragment())
+
+        bottomNavBar.setOnNavigationItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.home_page-> {
+                    replaceFragment(HomeFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.exercise_page -> {
+                    replaceFragment(ExerciseFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.cooking_page -> {
+                    replaceFragment(CookFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.test_page -> {
+                    replaceFragment(TestFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.setting_page -> {
+                    replaceFragment(SettingFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+                else -> {
+                    return@setOnNavigationItemSelectedListener false
+                }
+            }
+        }
     }
-    ///sdfsaf
-    ///sadf
-    fun init() {}
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentTransaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameLayout, fragment)
+        fragmentTransaction.commit()
+    }
 }
--- app/src/main/java/com/example/heolle_beoltteok/SplashActivity.kt
+++ app/src/main/java/com/example/heolle_beoltteok/SplashActivity.kt
@@ -2,20 +2,30 @@ package com.example.heolle_beoltteok
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import com.example.heolle_beoltteok.R
 
 class SplashActivity : AppCompatActivity() {
 
-    val SPLASH_VIEW_TIME:Long = 3000
+//    val SPLASH_VIEW_TIME:Long = 3000
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        Handler().postDelayed ({
-            startActivity(Intent(this,MainActivity::class.java))
+        val iv_bg = findViewById<ImageView>(R.id.imageView)
+
+
+        iv_bg.alpha = 0f
+        iv_bg.animate().setDuration(2000).alpha(1f).withEndAction {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
             finish()
-        },SPLASH_VIEW_TIME)
+        }
+
+//        Handler().postDelayed ({
+//            startActivity(Intent(this,MainActivity::class.java))
+//            finish()
+//        },SPLASH_VIEW_TIME)
     }
 }
