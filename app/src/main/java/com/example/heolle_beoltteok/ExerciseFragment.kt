b260e700de8fdc4125995d1d525102c6b8d0c941
coaspe <aspalt85@gmail.com>
2021-05-19 05:18:55 +0900

    Home Fragment. animation
    home fragment 디자인 빼고, transaction animation

--- app/src/main/java/com/example/heolle_beoltteok/ExerciseFragment.kt
+++ app/src/main/java/com/example/heolle_beoltteok/ExerciseFragment.kt
@@ -10,14 +10,14 @@ import androidx.fragment.app.Fragment
 
 class ExerciseFragment : Fragment() {
 
-
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_exercise, container, false)
         return inflater.inflate(R.layout.fragment_exercise, container, false)
     }
+
 
 }
