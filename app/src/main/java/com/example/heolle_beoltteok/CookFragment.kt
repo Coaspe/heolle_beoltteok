518a637953c51fbff3602db4eb8c836e334f7032
Your Name <you@your-domain.com>
2021-05-25 17:52:46 +0900

    no message

--- app/src/main/java/com/example/heolle_beoltteok/CookFragment.kt
+++ app/src/main/java/com/example/heolle_beoltteok/CookFragment.kt
@@ -5,17 +5,27 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.heolle_beoltteok.databinding.FragmentCookBinding
+import com.firebase.ui.database.FirebaseRecyclerOptions
+import com.google.firebase.database.FirebaseDatabase
+
+
 import kotlin.concurrent.thread
 
 
 class CookFragment : Fragment() {
+    lateinit var rdb: FirebaseDatabase
+    lateinit var adapter:MyAdapter
+
+
     var binding:FragmentCookBinding?=null
     var total = 0
     var started = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
 
     }
 
@@ -45,6 +55,9 @@ class CookFragment : Fragment() {
         }
 
     }
+
+
+
     fun pause() {
         started = false
 
@@ -58,12 +71,32 @@ class CookFragment : Fragment() {
 
     }
     fun init() {
+        rdb = FirebaseDatabase.getInstance()
+        val rdbref = rdb.getReference("MyDatas/items")
+
+
+
+        val query = rdbref.orderByKey()
+        val option = FirebaseRecyclerOptions.Builder<MyData>()
+            .setQuery(query, MyData::class.java)
+            .build()
+
+        adapter = MyAdapter(option)
+
+        binding!!.recyclerView.layoutManager = LinearLayoutManager(context,LinearLayoutManager.VERTICAL,false)
+        binding!!.recyclerView.adapter = adapter
+        adapter.startListening()
+
         total = binding!!.minute.text.toString().toInt() *3600 + binding!!.second.text.toString().toInt()*60 + binding!!.milli.text.toString().toInt()
-        binding!!.button5.setOnClickListener {
-            binding!!.minute.text = String.format("%02d",binding!!.button5.text.toString().toInt())
-            total = binding!!.minute.text.toString().toInt() *3600 + binding!!.second.text.toString().toInt()*60 + binding!!.milli.text.toString().toInt()
+
+        adapter.itemClickListener = object :MyAdapter.OnItemClickListener {
+            override fun OnItemClick(holder: MyAdapter.ViewHolder, view: View) {
+                binding!!.minute.text = String.format("%02d",holder.binding.time.text.toString().toInt())
+                total = binding!!.minute.text.toString().toInt() *3600 + binding!!.second.text.toString().toInt()*60 + binding!!.milli.text.toString().toInt()
+            }
         }
         binding!!.startBtn.setOnClickListener {
+
             start()
         }
         binding!!.pasueBtn.setOnClickListener {
