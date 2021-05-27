5f96977c6903fe7cc70a5f6aefdec1d170d70d04
yoon-youngjin <dudwls143@naver.com>
2021-05-27 18:34:16 +0900

    aaffsd

--- app/src/main/java/com/example/heolle_beoltteok/CookFragment.kt
+++ app/src/main/java/com/example/heolle_beoltteok/CookFragment.kt
@@ -5,37 +5,69 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.heolle_beoltteok.databinding.FragmentCookBinding
+import com.google.android.gms.tasks.OnSuccessListener
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.StorageReference
+import org.w3c.dom.Text
+import kotlin.concurrent.thread
 
 
 class CookFragment : Fragment() {
+    var binding:FragmentCookBinding?=null
     var total = 0
     var started = false
     var CookInfo_ArrayList : ArrayList<CookInfo> = ArrayList()
+    var flag = true
 
     lateinit var adapter: CookRecyclerViewAdapter
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.fragment_cook, container, false)
-        val recyclerView = view.findViewById<RecyclerView>(R.id.recyclerView)
-            firebaseDatainit()
-        initRecyclerView(recyclerView)
+        binding = FragmentCookBinding.inflate(layoutInflater,container,false)
+        //val recyclerView = view.findViewById<RecyclerView>(R.id.recyclerView)
+        firebaseDatainit()
+        initRecyclerView(binding!!.recyclerView)
         init()
-        return view
+        return binding!!.root
     }
     fun init() {
         adapter.itemClickListener = object : CookRecyclerViewAdapter.OnItemClickListener {
+
+
+
             override fun OnItemClick(holder: CookRecyclerViewAdapter.ViewHolder, view: View) {
 
-               var cookTimeText = holder.binding.CookingTime.text.toString()
-               val time =  cookTimeText.substring(0,cookTimeText.length-1)
-
+                var cookTimeText = holder.binding.CookingTime.text.toString()
+                val time = cookTimeText.substring(0, cookTimeText.length - 1)
+                binding!!.minute.text = String.format("%02d", time)
+                total = binding!!.minute.text.toString()
+                    .toInt() * 3600 + binding!!.second.text.toString()
+                    .toInt() * 60 + binding!!.milli.text.toString().toInt()
             }
         }
+
+            binding!!.startBtn.setOnClickListener {
+                if (flag == true) {
+                    start()
+                }
+            }
+
+            binding!!.pasueBtn.setOnClickListener {
+                pause()
+            }
+
+            binding!!.stopBtn.setOnClickListener {
+                stop()
+            }
+
+
+
     }
+
 
     private fun initRecyclerView(recyclerView: RecyclerView) {
         recyclerView.layoutManager = GridLayoutManager(context,2, GridLayoutManager.HORIZONTAL, false)
@@ -65,62 +97,44 @@ class CookFragment : Fragment() {
             Log.d("Exception", e.message.toString())
         }
     }
-//    fun start() {
-//        started = true
-//        //sub thread
-//        thread(start=true) {
-//            while(true) {
-//                Thread.sleep(1)
-//                if(!started)break
-//                total = total - 1
-//                activity!!.runOnUiThread {
-//
-//                    binding!!.minute.text = String.format("%02d",(total/3600)%60)
-//                    binding!!.second.text = String.format("%02d",(total/60)%60)
-//                    binding!!.milli.text = String.format("%02d",total%60)
-//                }
-//
-//            }
-//
-//        }
-//
-//    }
-//    fun pause() {
-//        started = false
-//
-//    }
-//    fun stop() {
-//        started = false
-//        total = 0
-//        binding!!.minute.text = "00"
-//        binding!!.second.text = "00"
-//        binding!!.milli.text = "00"
-//
-//    }
-//    fun init() {
-//    //recyclerview로 대체
-////        total = binding!!.minute.text.toString().toInt() *3600 + binding!!.second.text.toString().toInt()*60 + binding!!.milli.text.toString().toInt()
-////        binding!!.button5.setOnClickListener {
-////            binding!!.minute.text = String.format("%02d",binding!!.button5.text.toString().toInt())
-////            total = binding!!.minute.text.toString().toInt() *3600 + binding!!.second.text.toString().toInt()*60 + binding!!.milli.text.toString().toInt()
-////        }
-////        binding!!.startBtn.setOnClickListener {
-////
-////            start()
-////        }
-////        binding!!.pasueBtn.setOnClickListener {
-////            pause()
-////        }
-////        binding!!.stopBtn.setOnClickListener {
-////            stop()
-////        }
-//    }
-//    fun formatTime(time:Int) :String {
-//        val milisecond = String.format("%02d",time%60)
-//        val second = String.format("%02d",(time/60)%60)
-//        val minute = String.format("%02d",(time/3600)%60)
-//
-//        return "$minute : $second : $milisecond"
-//    }
+    fun start() {
+        started = true
+        //sub thread
+        thread(start=true) {
+            while(true) {
+                Thread.sleep(10)
+                if(!started)break
+                total = total - 1
+                activity!!.runOnUiThread {
 
+
+                    binding!!.minute.text = String.format("%02d",(total/3600)%60)
+                    binding!!.second.text = String.format("%02d",(total/60)%60)
+                    binding!!.milli.text = String.format("%02d",total%60)
+                }
+
+            }
+
+        }
+
+
+        flag = false
+
+    }
+    fun pause() {
+        started = false
+        flag = true
+
+    }
+    fun stop() {
+        started = false
+        total = 0
+        binding!!.minute.text = "00"
+        binding!!.second.text = "00"
+        binding!!.milli.text = "00"
+        flag = true
+
+    }
 }
+
+
